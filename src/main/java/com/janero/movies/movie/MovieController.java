4fc10d1259b3a382e25a13fdcf01f452e279474f
Eric Janero <ejanerop97@gmail.com>
2022-04-13 12:05:07 -0400

    Created list and get movies endpoints

--- src/main/java/com/janero/movies/movie/MovieController.java
+++ src/main/java/com/janero/movies/movie/MovieController.java
@@ -1,18 +1,53 @@
 package com.janero.movies.movie;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(path = "/movie")
 public class MovieController {
+    public static final int DEFAULT_PAGE_SIZE = 10;
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     @GetMapping()
-    public String getMovies(@RequestParam(required = false) String name) {
-        return "Movies controller: filtering by name: " + name;
+    public @ResponseBody Iterable<Movie> getMovies(@RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer year,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size) {
+
+        page = page == null ? 0 : page;
+        size = size == null ? DEFAULT_PAGE_SIZE : size;
+
+        Pageable pageable = PageRequest.of(page, size);
+        if (name == null) {
+            return movieRepository.findAll(pageable);
+        } else {
+            return movieRepository.findByNameContaining(name, pageable);
+        }
     }
+
+    @GetMapping(value = "/{id}")
+    public @ResponseBody Movie getMovie(@PathVariable Long id) {
+
+        try {
+            return movieRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found");
+        }
+    }
+
 
 
 }
--- src/main/java/com/janero/movies/movie/MovieRepository.java
+++ src/main/java/com/janero/movies/movie/MovieRepository.java
@@ -1,7 +1,11 @@
 package com.janero.movies.movie;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    Page<Movie> findByNameContaining(String name, Pageable pageable);
 
 }
